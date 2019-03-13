ce6b59eb25aa51688444c5d3fb3a915810dceef7
Ivica Matić <ivica1112@net.hr>
2019-03-13 14:21:46 +0000

    Create keysTEMPLATE.py

---
+++ portfolioWeb/portfolioWeb/keysTEMPLATE.py
@@ -0,0 +1,4 @@
+#keys are stored in this file, add this file later to gitigore
+
+secretkey = '8kwz5pk*q&htm5rd9&qj7oh2si%#%n!sjp7pne=3rumfyg*j)e'
+hosts = ['ivicamatic.co.uk','www.ivicamatic.co.uk','django.ivicamatic.co.uk','www.django.ivicamatic.co.uk']
