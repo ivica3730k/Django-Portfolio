6675c4fb0e885ba4621d8ec4617bdd4758e33f7d
Ivica Matić <ivica1112@net.hr>
2019-03-20 02:17:27 +0000

    Fix

--- portfolioWeb/mainPortfolio/views.py
+++ portfolioWeb/mainPortfolio/views.py
@@ -6,37 +6,20 @@ class indexData():
     def __init__(self):
         try:
             self.header = header.objects.latest('id')
-        except:
-            self.header = False
-        try:
             self.aboutMe = aboutMe.objects.latest('id')
-        except:
-            self.aboutMe = False
-        try:
             self.skills = skills.objects.order_by('id')
-        except:
-            self.skills = False
-        try:
             self.facts = facts.objects.order_by('id')
         except:
-            self.facts = False
-
+            self.header = self.aboutMe = self.skills = self.facts = False
     
 class aboutData():
     def __init__(self):
         try:
             self.aboutMe = aboutMe.objects.latest('id')
-        except:
-            self.aboutMe = False
-        try:
             self.expertises = expertises.objects.order_by('id')
-        except:
-            self.expertises = False
-        try:
             self.qualifications = qualifications.objects.order_by('id').reverse()
         except:
-            self.qualifications = False
-
+            self.aboutMe = self.expertises = self.qualifications = False
 
 
 
