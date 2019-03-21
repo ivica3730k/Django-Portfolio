1fba705255e0dc113465d54a50472962fdd3403c
Ivica Matić <ivica1112@net.hr>
2019-03-21 13:49:33 +0000

    Update views.py

--- portfolioWeb/mainPortfolio/views.py
+++ portfolioWeb/mainPortfolio/views.py
@@ -64,7 +64,7 @@ def about(request):
 
 def mywork(request):
     template = loader.get_template('mywork.html')
-    projectsList = projects.objects.order_by('order')
+    projectsList = projects.objects.order_by('order').reverse()
     context = {
         'projects':projectsList,
     }
